9a626a3e69ec3b154e983139da7f728a271956f3
Sanjay <sanjaym1998@gmail.com>
2023-02-25 16:18:53 +0530

    snapshot

--- src/components/Beachpage.jsx
+++ src/components/Beachpage.jsx
@@ -1,18 +1,40 @@
-// BeachPage.js
+import TopBar from "./topbar";
+import { useState } from "react";
+import "./index.css";
+import { useEffect } from "react";
+import axios from "axios"
 
-import React, { useContext } from 'react';
-import { PhotoContext } from './Photocontext';
-import PhotoGallery from './Photogalery';
-
-const BeachPage = () => {
-  const { photos } = useContext(PhotoContext);
-
-  return (
-    <div>
-      <h1>Beach Photos</h1>
-      <PhotoGallery photos={photos} />
-    </div>
-  );
-};
-
+const BeachPage=()=>{
+    const [data,setdata]=useState([]);
+    const [zoom,setZoom]=useState(Array(100).fill(false));
+    const zoomImage=(ind)=>{
+        zoom[ind]=true;
+        setZoom([...zoom]);
+       
+    }
+    const unzoomImage=(ind)=>{
+        zoom[ind]=false
+        setZoom([...zoom]);
+        
+    }
+    useEffect(()=>{
+        const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b8c77ace86b4ac039f289bfe356300db&tags=beaches&format=json&nojsoncallback=1';
+        axios.get(url).then((response)=>{setdata(response.data.photos.photo)}).catch((err)=>{console.log(err)});
+    },[])
+    
+    return(
+        <>
+        <TopBar/>
+        <h2>BeachPictures</h2>
+        <div id="imageContainer">
+              {data.map((value,index)=>{
+                const imgUrl=`https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}.jpg`
+                return(
+                    <img key={value.id} src={imgUrl} onMouseOver={()=>{zoomImage(index)}} onMouseLeave={()=>{unzoomImage(index)}}  className={zoom[index]?"img":"original"} alt="beachPictures"/>
+                )
+              })}
+        </div>
+        </>
+    )
+}
 export default BeachPage;
--- src/components/Searchpage.jsx
+++ src/components/Searchpage.jsx
@@ -1,43 +1,43 @@
-// SearchPage.js
+import TopBar from "./topbar";
+import { useState } from "react";
+import "./index.css";
+import { useEffect } from "react";
+import axios from "axios"
 
-import React, { useState, useContext } from 'react';
-import useHistory from 'react-router-dom';
-import { PhotoContext } from './Photocontext';
-import PhotoGallery from './Photogalery';
+const SearchPage=()=>{
+    const [data,setdata]=useState([]);
+    const [zoom,setZoom]=useState(Array(100).fill(false));
+    const [input,setInput]=useState("");
+    const [search,setsearch]=useState(false);
+    const zoomImage=(ind)=>{
+        zoom[ind]=true;
+        setZoom([...zoom]);
+       
+    }
+    const unzoomImage=(ind)=>{
+        zoom[ind]=false
+        setZoom([...zoom]);
+        
+    }
+    useEffect(()=>{
+        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b8c77ace86b4ac039f289bfe356300db&tags=${input}&format=json&nojsoncallback=1`;
+        axios.get(url).then((response)=>{setdata(response.data.photos.photo)}).catch((err)=>{console.log(err)});
+    },[search])
+    return(
 
-const SearchPage = () => {
-  const [query, setQuery] = useState('');
-  const { photos } = useContext(PhotoContext);
-  const history = useHistory();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    history.push(`/search?q=${query}`);
-    setQuery('');
-  };
-
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);
-  };
-
-  const searchResults = photos.filter((photo) =>
-    photo.title.toLowerCase().includes(query.toLowerCase())
-  );
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search photos by title..."
-          value={query}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Search</button>
-      </form>
-      <PhotoGallery photos={searchResults} />
-    </div>
-  );
-};
-
+        
+        <>
+        <TopBar function={setInput} trigger={setsearch} value={search}/>
+       
+        <div id="imageContainer">
+              {data.map((value,index)=>{
+                const imgUrl=`https://farm${value.farm}.staticflickr.com/${value.server}/${value.id}_${value.secret}.jpg`
+                return(
+                    <img key={value.id} src={imgUrl} onMouseOver={()=>{zoomImage(index)}} onMouseLeave={()=>{unzoomImage(index)}}  className={zoom[index]?"img":"original"} alt="mountainPictures"/>
+                )
+              })}
+        </div>
+        </>
+    )
+}
 export default SearchPage;
